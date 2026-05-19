9ab4c9bfca3f08e157189623d130934c522afc57
Sameer Basra <saimbasra939@gmail.com>
2026-05-19 06:13:55 +0500

    fix: open CORS for all origins

--- app/main.py
+++ app/main.py
@@ -6,13 +6,8 @@ app = FastAPI(title="AdOptimizer API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174", 
-        "http://localhost:5175",
-        "https://storied-bienenstitch-d70ff8.netlify.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
